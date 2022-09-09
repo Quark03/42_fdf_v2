08681ccbdc5552ad2acc12b1eadad595777ca561
Antonio Cinca <quark03.dev@gmail.com>
2022-09-09 14:56:39 +0100

    Norminette Check

--- src/parse.c
+++ src/parse.c
@@ -6,11 +6,25 @@
 /*   By: acinca-f <acinca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 08:57:58 by acinca-f          #+#    #+#             */
-/*   Updated: 2022/09/07 12:04:26 by acinca-f         ###   ########.fr       */
+/*   Updated: 2022/09/09 14:55:16 by acinca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
+
+void	parse_point(t_fdf *fdf, char **points, char **point, t_vect3 *temp)
+{
+	point = ft_split(points[fdf->map.cols++], ',');
+	temp->x = fdf->map.cols;
+	temp->y = fdf->map.rows;
+	temp->z = ft_atoi(point[0]);
+	if (point[1] != 0)
+		temp->color = ft_atoi_base((point[1] + 2), 16);
+	else
+		temp->color = 0x4287f5;
+	fdf->points3[fdf->points_size] = *temp;
+	fdf->points_size += 1;
+}
 
 void	parse_file(t_fdf *fdf)
 {
@@ -27,17 +41,7 @@ void	parse_file(t_fdf *fdf)
 		fdf->map.cols = 0;
 		while (points[fdf->map.cols] != 0)
 		{
-
-			point = ft_split(points[fdf->map.cols++], ',');
-			temp.x = fdf->map.cols;
-			temp.y = fdf->map.rows;
-			temp.z = ft_atoi(point[0]);
-			if (point[1] != 0)
-				temp.color = ft_atoi_base((point[1] + 2), 16);
-			else
-				temp.color = 0x4287f5;
-			fdf->points3[fdf->points_size] = temp;
-			fdf->points_size += 1;
+			parse_point(fdf, points, point, &temp);
 		}
 		line = get_next_line(fdf->file);
 		fdf->map.rows++;
