8f1f662f17fba291ac2e9f4ba0f0b2fea13a7c32
Antonio Cinca <quark03.dev@gmail.com>
2022-09-09 14:44:18 +0100

    Norminette Check

--- src/events.c
+++ src/events.c
@@ -6,13 +6,13 @@
 /*   By: acinca-f <acinca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 13:56:23 by acinca-f          #+#    #+#             */
-/*   Updated: 2022/09/09 14:26:51 by acinca-f         ###   ########.fr       */
+/*   Updated: 2022/09/09 14:44:06 by acinca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-int		close_window()
+int	close_window(void)
 {
 	exit(0);
 }
